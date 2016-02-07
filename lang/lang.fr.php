329beb740fbd72747d12d3bdba8865b4927d5f22
Nicolas Poitras <nicopoitras@gmail.com>
2016-02-07 13:30:21 -0500

    add language

--- lang/lang.fr.php
+++ lang/lang.fr.php
@@ -12,8 +12,21 @@ $langTab = array();
 $langTab['carrerTitle'] = 'Profil';
 $langTab['carrerText'] = 'Francais Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can <a href="http://themes.3rdwavemedia.com/website-templates/orbit-free-resume-cv-template-for-developers/" target="_blank">download this free resume/CV template here</a>. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.';
 
-$langTab['titre2'] = 'Expériences';
-$langTab['titre3'] = 'Projets';
-$langTab['titre4'] = 'Compétences';
+// experience
+
+$langTab['xpTitle'] = 'Expériences';
+$langTab['xpText'] = 'Francais Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can <a href="http://themes.3rdwavemedia.com/website-templates/orbit-free-resume-cv-template-for-developers/" target="_blank">download this free resume/CV template here</a>. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.';
+
+// projets
+
+$langTab['projectTitle'] = 'Projets';
+$langTab['projectText'] = 'Francais Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can <a href="http://themes.3rdwavemedia.com/website-templates/orbit-free-resume-cv-template-for-developers/" target="_blank">download this free resume/CV template here</a>. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.';
+
+// competences
+
+$langTab['compTitle'] = 'Compétences';
+$langTab['comp1'] = 'Expériences';
+$langTab['comp2'] = 'Projets';
+$langTab['comp3'] = 'Compétences';
 
 ?>
